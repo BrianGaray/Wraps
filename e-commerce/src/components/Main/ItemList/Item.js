984ac435fd34322c32a6760fe726bc50bf68d703
BrianGaray <brianchess18@gmail.com>
2023-04-02 05:45:43 -0300

    prueba

--- e-commerce/src/components/Main/ItemList/Item.js
+++ e-commerce/src/components/Main/ItemList/Item.js
@@ -5,34 +5,34 @@ import { Link } from "react-router-dom";
 const Item = ({ items }) => {
   
   return (
-    // <div class="grid grid-cols-1 gap-y-10 gap-x-6 mt-6 m-6">
-    //   <div class="collections relative">
-    //     <div class="img aspect-w-1 aspect-h-1 w-full overflow-hidden group-hover:opacity-75 lg:aspect-none">
-    //       <img className="m-auto" src={items.img} alt="Azuki" />
-    //     </div>
-    //     <div class="mt-4">
-    //       <h3 class="text-center text-base font-semibold">
-    //         <a href="#" class="block">
-    //           <span aria-hidden="true" class="absolute inset-0"></span>
-    //         </a>
-    //       </h3>
-    //     </div>
-    //   </div>
-    <section>
-      <div className="m-4">
-        <div className="card w-80 m-auto bg-base-100 shadow-xl">
+    <div>
+    {/* //carousel */}
+    {/* <div className="h-80 m-2 carousel carousel-vertical rounded-box">
+  <div className="carousel-item h-full">
+    <img src={items.img} />
+  </div> 
+  <div className="carousel-item h-full">
+    <img src={items.img} />
+  </div> 
+</div>
+  */}
+
+    {/* //cards  */}
+  
+      <div className="m-2 my-20">
+        <div className="card w-46 m-auto bg-base-100 shadow-xl">
           <figure>
-            <img src={items.img} alt="Shoes" />
+            <img className="p-2 rounded-3xl" src={items.img} alt="Shoes" />
           </figure>
-          <div className="card-body p-4">
+          <div className="card-body p-2">
             <h2 className="card-title">
              
               <div className="badge badge-primary">30% OFF</div>
             </h2>
 
             <div className="card-actions justify-end">
-              <div className="badge badge-outline">400$</div>
-              <div className="badge badge-outline">350$</div>
+              <div className="badge badge-ghost">400$</div>
+              <div className="badge badge-ghost">350$</div>
             </div>
           </div>
           <Link className="block text" to={`/item/${items.id}`}>
@@ -40,29 +40,9 @@ const Item = ({ items }) => {
           </Link>
         </div>
       </div>
-      <div className="m-4">
-        <div className="card w-80 m-auto bg-base-100 shadow-xl">
-          <figure>
-            <img src={items.img} alt="Shoes" />
-          </figure>
-          <div className="card-body p-4">
-            <h2 className="card-title">
-             
-              <div className="badge badge-primary">30% OFF</div>
-            </h2>
 
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">400$</div>
-              <div className="badge badge-outline">350$</div>
-            </div>
-          </div>
-          <Link className="block text" to={`/item/${items.id}`}>
-            <span aria-hidden="true" class="absolute inset-0"></span>
-          </Link>
-        </div>
-      </div>
- 
-    </section>
+
+    </div>
   );
 };
 
--- e-commerce/src/components/Main/ItemList/ItemList.js
+++ e-commerce/src/components/Main/ItemList/ItemList.js
@@ -3,7 +3,7 @@ import Items from "./Item";
 const ItemList = ({ items }) => {
   return (
     <section className="ItemList">
-      <div className="cards">
+      <div className="cards grid grid-cols-2 p-2">
         {items.map((items) => {
           return <Items items={items} key={items.id} />;
         })}
