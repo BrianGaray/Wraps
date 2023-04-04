5a4e8ec4c79f461772f63c5f771de5d6a06293b2
BrianGaray <brianchess18@gmail.com>
2023-04-04 18:51:29 -0300

    prueba

--- e-commerce/src/components/Main/ItemList/ItemList.js
+++ e-commerce/src/components/Main/ItemList/ItemList.js
@@ -2,10 +2,10 @@ import Items from "./Item";
 
 const ItemList = ({ items }) => {
   return (
-    <section className="ItemList p-4 gap-8 flex flex-col items-center justify-center">
+    <section className="ItemList p-4 gap-10 flex flex-col items-center justify-center">
       <div className="card bg-base-100 flex-row-reverse">
       <img
-            className="illustration w-5/12"
+            className="illustration"
             src="https://i.ibb.co/bb10p4j/g-06-Eu-V0-KETM-Rf-Y6p-JVe-transformed-1.png"
           />
       </div>
