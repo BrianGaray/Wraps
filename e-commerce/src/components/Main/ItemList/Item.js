f2c6e86bd0e74e803ea5a1f6a20f1121c65442fe
BrianGaray <brianchess18@gmail.com>
2023-04-04 13:13:30 -0300

    prueba

--- e-commerce/src/components/Main/ItemList/Item.js
+++ e-commerce/src/components/Main/ItemList/Item.js
@@ -10,46 +10,7 @@ const Item = ({ items }) => {
     
 
     {/* //cards  */}
-    <div className="m-2">
-        <div className="card w-46 m-auto bg-base-100 shadow-xl">
-          <figure>
-            <img className="p-2 rounded-3xl w-11/12" src={items.img} alt="Shoes" />
-          </figure>
-          <div className="card-body p-2">
-            <h2 className="card-title">
-             
-              {/* <div className="badge badge-primary">30% OFF</div> */}
-            </h2>
-
-            <div className="card-actions justify-end">
-              <div className="badge badge-ghost">400$</div>
-              <div className="badge badge-ghost">350$</div>
-            </div>
-          </div>
-          <Link className="block text" to={`/item/${items.id}`}>
-            <span aria-hidden="true" class="absolute inset-0"></span>
-          </Link>
-        </div>
-        <div className="card w-46 m-auto bg-base-100 shadow-xl">
-          <figure>
-            <img className="p-2 rounded-3xl w-11/12" src={items.img} alt="Shoes" />
-          </figure>
-          <div className="card-body p-2">
-            <h2 className="card-title">
-             
-              {/* <div className="badge badge-primary">30% OFF</div> */}
-            </h2>
-
-            <div className="card-actions justify-end">
-              <div className="badge badge-ghost">400$</div>
-              <div className="badge badge-ghost">350$</div>
-            </div>
-          </div>
-          <Link className="block text" to={`/item/${items.id}`}>
-            <span aria-hidden="true" class="absolute inset-0"></span>
-          </Link>
-          
-        </div>
+    <div className="">
         <div className="card w-46 m-auto bg-base-100 shadow-xl">
           <figure>
             <img className="p-2 rounded-3xl w-11/12" src={items.img} alt="Shoes" />
--- e-commerce/src/components/Main/ItemList/ItemList.js
+++ e-commerce/src/components/Main/ItemList/ItemList.js
@@ -2,42 +2,16 @@ import Items from "./Item";
 
 const ItemList = ({ items }) => {
   return (
-    <section className="ItemList">
-      <div className="hero mt-20 mb-4 bg-base-200">
-        <div className="hero-content gap-8 flex-row-reverse">
-          <img
-            className="w-2/5"
+    <section className="ItemList p-4 gap-8 flex flex-col items-center justify-center">
+      <div className="card bg-base-100 flex-row-reverse">
+      <img
+            className="illustration w-2/5"
             src="https://i.ibb.co/bb10p4j/g-06-Eu-V0-KETM-Rf-Y6p-JVe-transformed-1.png"
           />
-
-          <div className="w-64 carousel rounded-box">
-            <div className="carousel-item w-full">
-              <img
-                src="https://i.ibb.co/LvVkK16/Dise-o-sin-t-tulo-1.png"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="https://i.ibb.co/LvVkK16/Dise-o-sin-t-tulo-1.png"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-            <div className="carousel-item w-full">
-              <img
-                src="https://i.ibb.co/LvVkK16/Dise-o-sin-t-tulo-1.png"
-                className="w-full"
-                alt="Tailwind CSS Carousel component"
-              />
-            </div>
-          </div>
-        </div>
       </div>
-      <div className="divider m-2"></div>
+     
 
-      <div className="cards grid grid-cols-2 p-2">
+      <div className="cards grid grid-cols-2 gap-4">
         {items.map((items) => {
           return <Items items={items} key={items.id} />;
         })}
