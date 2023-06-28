4bf640859513e87ae8c4deaf467e068f19973aeb
BrianGaray <brianchess18@gmail.com>
2023-06-28 06:39:49 -0300

    prueba

--- e-commerce/src/components/pages/home/loader.js
+++ e-commerce/src/components/pages/home/loader.js
@@ -4,9 +4,9 @@ const Loader = () => {
   return (
     <div>
       <div className="flex items-center justify-center h-full w-full absolute top-0 left-0 space-x-2">
-        <div className="w-4 h-4 rounded-full animate-pulse bg-base-300"></div>
-        <div className="w-4 h-4 rounded-full animate-pulse bg-base-300"></div>
-        <div className="w-4 h-4 rounded-full animate-pulse bg-base-300"></div>
+        <div className="w-4 h-4 rounded-full animate-pulse bg-base-content"></div>
+        <div className="w-4 h-4 rounded-full animate-pulse bg-base-content"></div>
+        <div className="w-4 h-4 rounded-full animate-pulse bg-base-content"></div>
       </div>
     </div>
   );
